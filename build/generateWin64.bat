e17f8917163d3a52e5a02b73fb82cffa77370007
Vladislav Nikolaev <yavl98@mail.ru>
2024-11-24 02:43:10 +0300

    Add freetype includes, remove spdlog

--- build/generateWin64.bat
+++ build/generateWin64.bat
@@ -1,3 +1,3 @@
-cmake -G "Visual Studio 16 2019" -DCMAKE_CXX_FLAGS="/MP" -A x64 .
+cmake -G "Visual Studio 17 2022" -DCMAKE_CXX_FLAGS="/MP" -A x64 .
 xcopy /s "..\extlibs\libs-msvc\x86" ".\"
 xcopy /s "..\assets" ".\assets\"
--- src/trew/Logger.cpp
+++ src/trew/Logger.cpp
@@ -1,18 +1,10 @@
 #include "Logger.hpp"
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
+#define FMT_HEADER_ONLY
+#include <fmt/core.h>
 
 using namespace trew;
 
 Logger::Logger(bool overwriteLogfile) : logLevel(LogLevel::LOG_DEBUG) {
-	spdlog::set_level(spdlog::level::debug);
-	try {
-		auto async_file = spdlog::basic_logger_mt("async_file_logger", "trewlib.log", overwriteLogfile);
-		spdlog::set_default_logger(async_file);
-	}
-	catch (spdlog::spdlog_ex& e) {
-		fmt::print("{}\n", e.what());
-	}
 }
 
 Logger::~Logger() {
@@ -26,42 +18,37 @@ Logger& Logger::getInstance() {
 void Logger::log(std::string tag, std::string message) {
 	if (logLevel == LogLevel::LOG_NONE || logLevel == LogLevel::LOG_ERROR)
 		return;
-	spdlog::info("[{}] {}", tag, message);
+	fmt::print("[{}] {}", tag, message);
 }
 
 void Logger::error(std::string tag, std::string message) {
 	if (logLevel == LogLevel::LOG_NONE)
 		return;
-	spdlog::info("[{}] {}", tag, message);
+	fmt::print("[{}] {}", tag, message);
 }
 
 void Logger::debug(std::string tag, std::string message) {
 	if (logLevel == LogLevel::LOG_NONE || logLevel == LogLevel::LOG_INFO)
 		return;
-	spdlog::debug("[{}] {}", tag, message);
+	fmt::print("[{}] {}", tag, message);
 }
 
 void Logger::setLogLevel(LogLevel logLevel) {
 	this->logLevel = logLevel;
 	switch (logLevel) {
 	case LogLevel::LOG_DEBUG: {
-		spdlog::set_level(spdlog::level::debug);
 		break;
 	}
 	case LogLevel::LOG_ERROR: {
-		spdlog::set_level(spdlog::level::err);
 		break;
 	}
 	case LogLevel::LOG_INFO: {
-		spdlog::set_level(spdlog::level::info);
 		break;
 	}
 	case LogLevel::LOG_NONE: {
-		spdlog::set_level(spdlog::level::off);
 		break;
 	}
 	default: {
-		spdlog::set_level(spdlog::level::debug);
 		break;
 	}
 	}
