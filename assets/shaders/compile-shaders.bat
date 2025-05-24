86c517e09324def756b0af87ea71f7494e14aa38
Vladislav Nikolaev <yavl98@mail.ru>
2025-05-24 22:00:26 +0300

    Add compile-shaders.bat

---
+++ assets/shaders/compile-shaders.bat
@@ -0,0 +1,2 @@
+slangc RawTriangle.vert.slang -target spirv -o compiled/RawTriangle.vert.spv
+slangc SolidColor.frag.slang -target spirv -o compiled/SolidColor.frag.spv
--- build/generateWin64.bat
+++ build/generateWin64.bat
@@ -1,3 +1,5 @@
 cmake -G "Visual Studio 17 2022" -DCMAKE_CXX_FLAGS="/MP /utf-8" -A x64 .
 xcopy /s "..\extlibs\libs-msvc\x64" ".\"
 xcopy /s "..\assets" ".\assets\"
+cd assets/shaders/
+./compile-shaders.bat
